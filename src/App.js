5db23f046559c963cacbe76d449dac1c6bfa72a1
Jerry Ogunbor <ogunborjerry@gmail.com>
2022-10-05 13:32:01 +0100

    created the navbar.js, navbar.css also added it to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // page components
+import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Search from "./pages/search/Search";
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
