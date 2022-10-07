baad3677bfc2f9fb0c968b35b6212516b1cd19c4
Jerry Ogunbor <ogunborjerry@gmail.com>
2022-10-07 10:59:24 +0100

    See readme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useTheme } from "./hooks/useTheme";
 
 // page components
 import Navbar from "./components/Navbar";
@@ -6,15 +7,18 @@ import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Search from "./pages/search/Search";
 import Recipe from "./pages/recipe/Recipe";
+import ThemeSelector from "./components/ThemeSelector";
 
 // styles
 import "./App.css";
 
 function App() {
+  const { mode } = useTheme();
   return (
-    <div className="App">
+    <div className={`App ${mode}`}>
       <BrowserRouter>
         <Navbar />
+        <ThemeSelector />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
